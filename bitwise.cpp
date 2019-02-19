0ee1cc6391ff39926e3c5eaf1b8a3d40c8e18fc3
Anand <anandshastry88@gmail.com>
2019-02-19 19:04:33 +0530

    Update bitwise.cpp

--- bitwise.cpp
+++ bitwise.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()
 {
     unsigned int y=0xFF00FF00;
-    std::bitset<32> x(y);
+    std::bitset<32> x(y); // to print in binary
     cout<<x<<endl;
-    x=y^0xFFFFFFFF;
+    x=y^0xFFFFFFFF; //  x^1s = ~x
     //std::bitset<32> x(y);
     cout<<x<<endl;
     return 0;
